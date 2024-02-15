544dfdb4bc1354a9e79597cf205fefc11d9ca534
dannycarbonero <danny.carbonero@protonmail.com>
2024-02-15 10:10:10 -0500

    update

--- contributors/Danny/make_silver_priors_py_files.py
+++ contributors/Danny/make_silver_priors_py_files.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 import pandas as pd
 import pickle as pkl
@@ -16,14 +18,14 @@ time = []
 classifications = []
 
 data_path = get_parent_path('data', 'Spike Ripples/silver/priors_csvs')
-
+save_path = get_parent_path('data', 'Spike Ripples/silver')
 for prefix in file_prefixes:
 
     ripple_params.append(pd.read_csv(data_path + prefix + '_rippleparams.csv'))
     ripple_times.append(pd.read_csv(data_path + prefix + '_rippletimes.csv'))
     spike_params.append(pd.read_csv(data_path + prefix + '_spikeparams.csv'))
     spike_times.append(pd.read_csv(data_path + prefix + '_spiketimes.csv'))
-    series.append(np.array(pd.read_csv(data_path + prefix + '_series.csv', header = None)))
+    series.append(pd.read_csv(data_path + prefix + '_series.csv', header = None))
     working_time.append(pd.read_csv(data_path+ prefix + '_time.csv', header = None))
 
 for i in range(len(file_prefixes)):
@@ -36,3 +38,13 @@ spike_params = pd.concat(spike_params)
 spike_times = pd.concat(spike_times)
 series = np.vstack(series)
 time = np.vstack(time)
+ripple_times.columns = ['ripple_start','spike_stop']
+spike_times.columns = ['spike_start', 'spike_stop']
+
+master_frame = pd.concat([ripple_params, ripple_times, spike_params, spike_times], axis = 1)
+master_frame['classification'] = classifications
+master_frame['series'] = series.tolist()
+master_frame['time'] = time.tolist()
+
+with open(save_path + 'silver_priors_data_frame.pkl', 'wb') as file:
+    pkl.dump(master_frame, file)
