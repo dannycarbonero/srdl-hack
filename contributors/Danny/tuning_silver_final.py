19ceecd5d1e0020e9c1534da9e73a25512e88633
danny.carbonero <danny.carbonero@protonmail.com>
2024-02-16 13:40:48 -0500

    update

--- contributors/Danny/tuning_silver_final.py
+++ contributors/Danny/tuning_silver_final.py
@@ -1,8 +1,7 @@
-import numpy as np
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import h5py
@@ -12,7 +11,7 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 from directory_handling import get_parent_path
-from utilities import binarize_classifications, make_refined_labels, create_training_subset, generate_LOO_subjects
+from utilities import binarize_classifications, make_refined_labels, create_training_subset, generate_LOO_subjects, load_RippleNet
 
 #%% load Our Data
 silver_Fs = 2035 # from simulation
@@ -23,7 +22,6 @@ with open(data_path + 'silver_data_frame.pkl', 'rb') as file:
 
 #%% define LOO subjects
 LOO_subjects = generate_LOO_subjects()
-# LOO_subjects = ['pBECTS015']
 
 #%% a few constants
 cut_fractor = 0.75
@@ -37,34 +35,29 @@ post_center_s = 0.05
 batch_size = 32
 epochs = 128
 
-network_directory = get_parent_path('data', subdirectory = 'Spike Ripples/silver/RippleNet_tuned_LOO_' + str(epochs) + '_epochs_final/', make = True)
+network_directory = get_parent_path('data', subdirectory = 'Spike Ripples/silver/RippleNet_tuned_LOO_' + str(epochs) + '_epochs_val_1/', make = True)
 
 #%% train
 for i, subject in zip(range(len(LOO_subjects)), LOO_subjects):
 
-    #load their model
-    RippleNet_path = get_parent_path('scc', subdirectory='RippleNet')
-    model_file = RippleNet_path + 'best_model.pkl'
-    with open(model_file, 'rb') as f:
-        best_model = pickle.load(f)
-        print(best_model)
-
-    model = keras.models.load_model(RippleNet_path + best_model['model_file'])
-    model.summary()
-
+    model = load_RippleNet('scc')
 
     print('Training on subject %i of %i' %(i, len(LOO_subjects)))
 
-    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(network_directory + 'RippleNet_tuned_optimal_' + subject + '.h5', monitor='loss', verbose=1, save_best_only=True, mode='min')
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(network_directory + 'RippleNet_tuned_optimal_' + subject + '.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
     checkpoint_history = keras.callbacks.CSVLogger(network_directory + 'RippleNet_tuning_history_' + subject + '.csv')
     checkpoint_list = [model_checkpoint, checkpoint_history]
 
-    # pull data
-    # validation_frame = data.copy()[data['subject'] == subject]
-    training_frame = data.copy()[data['subject']!=subject]
+
+    # pull validation data
+    LOO_frame = data.copy()[data['subject']!=subject]
+    frame_y = LOO_frame[LOO_frame['classification'] == 'y']
+    validation_frame = pd.concat((LOO_frame[LOO_frame['classification'] == 'y'].sample(int(frame_y.shape[0] * 0.1)), (LOO_frame[LOO_frame['classification'] == 'n'].sample(int(frame_y.shape[0] * 0.05))),(LOO_frame[LOO_frame['classification'] == 'bk'].sample(int(frame_y.shape[0] * 0.05)))))
+    training_frame = LOO_frame.loc[LOO_frame.index.difference(validation_frame.index)]
+
     training_frame = create_training_subset(training_frame, int(training_frame['classification'].value_counts()['y'] * 2))
 
-    # training data
+    # training data processing
     training_series = np.stack(np.array(training_frame.series))[:, cut_points:-cut_points]
     training_time = np.stack(np.array(training_frame.time))
     training_time = training_time - training_time[:,0].reshape(-1,1)
@@ -77,24 +70,24 @@ for i, subject in zip(range(len(LOO_subjects)), LOO_subjects):
         training_classifications = np.delete(training_classifications, nan_series, axis=0)
     training_classifications_bin = binarize_classifications(training_classifications)
 
-    # validation data
-    # validation_series = np.stack(np.array(validation_frame.series))[:, cut_points:-cut_points]
-    # validation_time = np.stack(np.array(validation_frame.time))
-    # validation_time = validation_time - validation_time[:,0].reshape(-1,1)
-    # validation_time = validation_time[:, cut_points:-cut_points]
-    # validation_classifications = np.array(validation_frame.classification)
-    # nan_series = np.where(np.any(np.isnan(validation_series), axis=1))[0]
-    # if np.any(nan_series):
-    #     validation_series = np.delete(validation_series, nan_series, axis=0)
-    #     validation_time = np.delete(validation_time, nan_series, axis=0)
-    #     validation_classifications = np.delete(validation_classifications, nan_series, axis=0)
-    # validation_classifications_bin = binarize_classifications(validation_classifications)
+    # validation data processing
+    validation_series = np.stack(np.array(validation_frame.series))[:, cut_points:-cut_points]
+    validation_time = np.stack(np.array(validation_frame.time))
+    validation_time = validation_time - validation_time[:,0].reshape(-1,1)
+    validation_time = validation_time[:, cut_points:-cut_points]
+    validation_classifications = np.array(validation_frame.classification)
+    nan_series = np.where(np.any(np.isnan(validation_series), axis=1))[0]
+    if np.any(nan_series):
+        validation_series = np.delete(validation_series, nan_series, axis=0)
+        validation_time = np.delete(validation_time, nan_series, axis=0)
+        validation_classifications = np.delete(validation_classifications, nan_series, axis=0)
+    validation_classifications_bin = binarize_classifications(validation_classifications)
 
     # downsample
     training_series_downsampled = np.expand_dims(signal.resample(training_series, int(RippleNet_Fs / silver_Fs * training_series.shape[1]), axis=1), 2)
     training_time_downsampled = signal.resample(training_time, int(RippleNet_Fs / silver_Fs * training_time.shape[1]), axis = 1)
-    # validation_series_downsampled = np.expand_dims(signal.resample(validation_series, int(RippleNet_Fs / silver_Fs * validation_series.shape[1]), axis=1), 2)
-    # validation_time_downsampled = signal.resample(training_time, int(RippleNet_Fs / silver_Fs * validation_time.shape[1]), axis = 1)
+    validation_series_downsampled = np.expand_dims(signal.resample(validation_series, int(RippleNet_Fs / silver_Fs * validation_series.shape[1]), axis=1), 2)
+    validation_time_downsampled = signal.resample(training_time, int(RippleNet_Fs / silver_Fs * validation_time.shape[1]), axis = 1)
 
     # make labels
     training_labels = np.expand_dims(make_refined_labels(training_classifications, training_time_downsampled, center_s = label_center_s, pre_center_s = pre_center_s, post_center_s = post_center_s), 2)
@@ -105,11 +98,11 @@ for i, subject in zip(range(len(LOO_subjects)), LOO_subjects):
     training_set = training_set.shuffle(training_series_downsampled.shape[0])
     training_set = training_set.batch(batch_size)
 
-    # validation_set = tf.data.Dataset.from_tensor_slices((validation_series_downsampled, validation_labels))
-    # validation_set = validation_set.shuffle(validation_series_downsampled.shape[0])
+    validation_set = tf.data.Dataset.from_tensor_slices((validation_series_downsampled, validation_labels))
+    validation_set = validation_set.shuffle(validation_series_downsampled.shape[0])
 
 
-    history = model.fit(training_set, epochs = epochs, callbacks = checkpoint_list)#, validation_data=validation_set)
+    history = model.fit(training_set, epochs = epochs, callbacks = checkpoint_list, validation_data=validation_set)
     with open(network_directory + 'RippleNet_tuning_history_' + subject +'.pkl', 'wb') as file:
         pickle.dump(history.history, file)
 
