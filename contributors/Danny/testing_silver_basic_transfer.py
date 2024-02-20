0169c28b2f534697fcb01386284ce14cec442cc0
dannycarbonero <danny.carbonero@protonmail.com>
2024-02-20 13:43:21 -0500

    update:

--- contributors/Danny/testing_silver_basic_transfer.py
+++ contributors/Danny/testing_silver_basic_transfer.py
@@ -9,96 +9,147 @@ import h5py
 import pickle
 import pandas as pd
 from scipy import signal
-import matplotlib.pyplot as plt
 from sklearn import metrics
 
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Arial'
+
 from directory_handling import get_parent_path
-from utilities import binarize_classifications, refined_classification, make_refined_labels, generate_LOO_subjects
+from utilities import generate_LOO_subjects, pull_event_probabilities, build_data_sets, find_optimum_ROC_threshold, classify_continuous_predictions, calculate_prediction_statistics, load_RippleNet
 from pathlib import Path
-
-
-#%% load their model
-RippleNet_path = get_parent_path('code', subdirectory = 'RippleNet')
-model_file = RippleNet_path + 'best_model.pkl'
-with open(model_file, 'rb') as f:
-    best_model = pickle.load(f)
-    print(best_model)
-
-model = keras.models.load_model(RippleNet_path + best_model['model_file'])
-model.summary()
-
 
 #%% load Our Data
 silver_Fs = 2035 # from simulation
 data_directory = get_parent_path('data', subdirectory = 'Spike Ripples/silver')
 
-LOO_subjects = generate_LOO_subjects()
-
 with open(data_directory + 'silver_data_frame.pkl', 'rb') as file:
     data = pickle.load(file)
 
-figure_directory ='figures/'
-Path(figure_directory).mkdir(exist_ok = True)
 
+#%%
+#LOO_subjects = [generate_LOO_subjects()[-1]] # LOO for current augmenting is 43
+LOO_subjects = generate_LOO_subjects()
 
 #%%
 # some constants
-cut_fractor = 0.75
-cut_points = int(silver_Fs * cut_fractor)
+cut_factor = 0.75
+cut_points = int(silver_Fs * cut_factor)
 RippleNet_Fs = 1250
 label_center_s = 1
 pre_center_s = 0.1
 post_center_s = 0.05
+window_bounds = [label_center_s - pre_center_s, label_center_s + post_center_s]
 
 # prediction params
-height = 0.75
 width_s = .025
 distance_s = .1
 width = int(RippleNet_Fs * width_s)
 distance = int(RippleNet_Fs * distance_s)
 
+paired_classifications = []
+predictions_bin = []
+optimal_thresholds = []
+ROC_statistics = []
+confusion_matrices = []
+classifications = []
+event_probabilities = []
+labels = []
+predictions_aggregate = []
 
-#%% process data for predicting
-
-testing_data = data[data['subject'].isin(LOO_subjects)]
-series = np.stack(np.array(testing_data.series))
-time = np.stack(np.array(testing_data.time))
-time = time - time[:,0].reshape(-1, 1)
-classifications = np.array(testing_data.classification)
-
-series = series[:, cut_points:-cut_points]
-time = time[:, cut_points:-cut_points]
-nan_series = np.where(np.any(np.isnan(series), axis=1))[0]
-if np.any(nan_series):
-    series = np.delete(series, nan_series, axis = 0)
-    time = np.delete(series, nan_series, axis = 0)
-    classifications = np.delete(classifications, nan_series, axis = 0)
-classifications_bin = binarize_classifications(classifications)
-
-series_downsampled = signal.resample(series, int(RippleNet_Fs/silver_Fs * series.shape[1]), axis = 1)
-time_downsampled = signal.resample(time, int(RippleNet_Fs/silver_Fs * series.shape[1]), axis = 1)
-labels = make_refined_labels(classifications, time_downsampled, center_s = label_center_s, pre_center_s = pre_center_s, post_center_s = post_center_s)
+model = load_RippleNet('code')
 
 
-#%% predict
-predictions = model.predict(np.expand_dims(series_downsampled, axis = 2).squeeze())
-predictions = predictions.squeeze()
+for subject in LOO_subjects:
 
 
-#%% assess predictions
-prediction_peaks = []
-for i in range(predictions.shape[0]):
-    prediction_peaks.append(signal.find_peaks(predictions[i,:], height = height, width = width, distance = distance)[0])
-paired_classifications, predictions_bin = refined_classification(prediction_peaks, classifications_bin, labels)
+    testing_frame = data.copy()[data['subject']==subject]
+    _, testing_data = build_data_sets(testing_frame,  cut_factor = cut_factor, silver_Fs = silver_Fs, RippleNet_Fs = RippleNet_Fs, label_center_s = label_center_s, pre_center_s = pre_center_s, post_center_s = post_center_s)
+    predictions = model.predict(np.expand_dims(testing_data['series_downsampled'], axis = 2)).squeeze()
+    probabilities = pull_event_probabilities(predictions, testing_data['time_downsampled'], window_bounds)
+    optimal_probability_threshold,_ = find_optimum_ROC_threshold(probabilities, testing_data['classifications'])
+    event_probabilities.append(probabilities)
+    predictions_aggregate.append(predictions)
+
+    paired_classifications_working, predictions_bin_working = classify_continuous_predictions(predictions, testing_data['classifications'], testing_data['labels'], optimal_probability_threshold, width, distance)
+    ROC_statistics.append(metrics.roc_curve(testing_data['classifications'], probabilities))
 
 
-#%% plot
-plt.figure()
-confusion_matrix = metrics.confusion_matrix(paired_classifications, predictions_bin)
-cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
-cm_display.plot()
-plt.title('Un-tuned Transfer Learning')
-plt.savefig(figure_directory + 'un-tuned_transfer_LOO.png')
-plt.show()
+    confusion_matrices.append(metrics.confusion_matrix(paired_classifications_working, predictions_bin_working).ravel())  # tn, fp, fn, tp
+
+    classifications.append(testing_data['classifications'])
+    labels.append(testing_data['labels'])
+
+
+    paired_classifications.append(paired_classifications_working)
+    predictions_bin.append(predictions_bin_working)
+
+#%% cummulative statistics
+optimal_probability_threshold_cum, operating_point_cum = find_optimum_ROC_threshold(np.concatenate(event_probabilities),np.concatenate(classifications))
+ROC_curve_cum = metrics.roc_curve(np.concatenate(classifications), np.concatenate(event_probabilities))
+paired_classifications_cum, predictions_bin_cum = classify_continuous_predictions(np.vstack(predictions_aggregate), np.concatenate(classifications), np.vstack(labels), optimal_probability_threshold_cum, width, distance)
+
+
+paired_classifications_50, predictions_bin_50 = classify_continuous_predictions(np.vstack(predictions_aggregate), np.concatenate(classifications), np.vstack(labels), 0.5, width, distance)
+
+sens_agg, spec_agg, ppv_agg, npv_agg = calculate_prediction_statistics(np.concatenate(paired_classifications), np.concatenate(predictions_bin))
+sens_opt, spec_opt, ppv_opt, npv_opt = calculate_prediction_statistics(paired_classifications_cum, predictions_bin_cum)
+sens_50, spec_50, ppv_50, npv_50 = calculate_prediction_statistics(paired_classifications_50, predictions_bin_50)
+prediction_statistics = np.vstack(((sens_50, spec_50, ppv_50, npv_50), (sens_agg, spec_agg, ppv_agg, npv_agg), (sens_opt, spec_opt, ppv_opt, npv_opt)))
+
+
+#%% plotting
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# for i in range(len(ROC_statistics)):
+#     ax.plot(ROC_statistics[i][0], ROC_statistics[i][1], alpha = 0.66)
+# ax.plot(ROC_curve_cum[0], ROC_curve_cum[1], color = 'k')
+# ax.plot(np.linspace(-1,2,100), np.linspace(-1,2,100), color = 'k', linestyle = '--')
+# ax.set_ylim([-0.05,1.05])
+# ax.set_xlim([-0.05,1.05])
+# ax.scatter(operating_point_cum[0], operating_point_cum[1], color = 'k')
+# ax.set_xlabel('False Positive Rate')
+# ax.set_ylabel('True Positive Rate')
+# ax.spines[['right', 'top']].set_visible(False)
+# fig.show()
+#
+# columns = ['Sensitivity', 'Specificity', 'PPV', 'NPV']
+# rows = ['$p_{0.5}$', '$p_{th}$', '$p_{opt}$']
+# prediction_statistics = np.vstack(((sens_50, spec_50, ppv_50, npv_50), (sens_agg, spec_agg, ppv_agg, npv_agg), (sens_opt, spec_opt, ppv_opt, npv_opt)))
+# fig1 = plt.figure()
+# ax = fig1.add_subplot(111)
+# ax.table(cellText=prediction_statistics, rowLabels=rows, colLabels=columns, loc='center', cellLoc='center')
+
+fig = plt.figure(figsize=(10/1.5, 8/1.5))
+
+# Adding ROC plot
+ax_roc = plt.subplot2grid((4, 1), (0, 0), rowspan=3, fig=fig)  # Allocate 3/4 of the figure to ROC
+for i in range(len(ROC_statistics)):
+    ax_roc.plot(ROC_statistics[i][0], ROC_statistics[i][1], alpha=0.66)
+ax_roc.plot(ROC_curve_cum[0], ROC_curve_cum[1], color='k')
+ax_roc.plot(np.linspace(-1, 2, 100), np.linspace(-1, 2, 100), color='k', linestyle='--')
+ax_roc.set_ylim([-0.05, 1.05])
+ax_roc.set_xlim([-0.05, 1.05])
+ax_roc.scatter(operating_point_cum[0], operating_point_cum[1], color='k')
+ax_roc.set_xlabel('False Positive Rate')
+ax_roc.set_ylabel('True Positive Rate')
+ax_roc.spines[['right', 'top']].set_visible(False)
+
+columns = ['Sensitivity', 'Specificity', 'PPV', 'NPV']
+rows = ['$p_{0.5}$', '$p_{th}$', '$p_{opt}$']
+prediction_statistics = np.vstack(((sens_50, spec_50, ppv_50, npv_50), (sens_agg, spec_agg, ppv_agg, npv_agg), (sens_opt, spec_opt, ppv_opt, npv_opt)))
+formatted_prediction_statistics = [[f'{value:.4f}' for value in row] for row in prediction_statistics]
+
+# Adding the table at the bottom, taking up 25% of the figure
+ax_table = plt.subplot2grid((4, 1), (3, 0), fig=fig)
+ax_table.axis('tight')
+ax_table.axis('off')
+tbl = ax_table.table(cellText=formatted_prediction_statistics, rowLabels=rows, colLabels=columns, loc='center', cellLoc='center')
+tbl.auto_set_font_size(False)
+tbl.set_fontsize(10)
+tbl.scale(1, 1.5)  # You may adjust these scaling factors as needed
+
+plt.tight_layout()
+fig.show()
 
 #%% THEIR DATA - check RippleNet_path/RippleNet_interactive_prototype.ipynb
