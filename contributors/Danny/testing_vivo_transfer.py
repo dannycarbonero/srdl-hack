33381277a1566b3210bea81332dc410d78ac29b3
dannycarbonero <danny.carbonero@protonmail.com>
2024-02-21 13:51:20 -0500

    update

--- contributors/Danny/testing_vivo_transfer.py
+++ contributors/Danny/testing_vivo_transfer.py
@@ -26,7 +26,7 @@ data_directory = get_parent_path('data', subdirectory = 'Spike Ripples/silver')
 with open(data_directory + 'silver_data_frame.pkl', 'rb') as file:
     data = pickle.load(file)
 
-network_directory = get_parent_path('data', subdirectory = 'Spike Ripples/silver/RippleNet_tuned_LOO_128_epochs_val_2b/lr_decrease')
+network_directory = get_parent_path('data', subdirectory = 'Spike Ripples/silver/RippleNet_tuned_LOO_128_epochs_val_2b/lr_reinc')
 # figure_directory ='figures/LOO_tuning_val_1/'
 # Path(figure_directory).mkdir(exist_ok = True)
 
